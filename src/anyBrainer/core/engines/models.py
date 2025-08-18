bug fix in metrics calculation

src/anyBrainer/core/engines/models.py
@@ -723,11 +723,14 @@ class SegmentationModel(BaseModel):
     @torch.no_grad()
     def compute_metrics(self, out: torch.Tensor, target: torch.Tensor) -> dict[str, Any]:
         """Computes metrics; ignores if a metric fails."""
-        # Convert to one-hot for uniform API across metrics and tasks.
-        if out.ndim == target.ndim + 1:
-            target = cast(torch.Tensor, AsDiscrete(to_onehot=out.shape[1])(target)) # Assumes (B, C, ...)
-        elif out.ndim == target.ndim:
+        # Convert everything to one-hot for uniform API across metrics and tasks; 
+        # assumes (B, C, ...)
+        if out.shape[1] == target.shape[1]:
             pass
+        elif out.shape[1] == 1: # one-hot labels and continuous output; convert out to one-hot
+            out = cast(torch.Tensor, AsDiscrete(argmax=True, to_onehot=target.shape[1])(out))
+        elif target.shape[1] == 1: # continuous output and one-hot labels; convert target to one-hot
+            target = cast(torch.Tensor, AsDiscrete(to_onehot=out.shape[1])(target))
         else:
             msg = (f"[{self.__class__.__name__}.compute_metrics] Unexpected output "
                    f"and target shapes: {out.shape} and {target.shape}.")